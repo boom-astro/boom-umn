b24b3587d690aa88ac468699fc79a2c3a8cc4270
antoine-le-calloch <antoine.lecalloch@gmail.com>
2025-10-19 16:07:03 -0500

    Add SLURM job script to daily consume ZTF alerts using scrontab.

---
+++ consume_ztf_daily.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --job-name=boom_slurm_job
+#SBATCH --partition boom
+#SBATCH --time=0-23:00:00
+#SBATCH --ntasks=35
+
+cd boom || exit 1
+mkdir -p logs/slurm
+
+exec >"logs/slurm/consume_ztf_daily_$(date -u "+%Y-%m-%d")_%j.out" 2>&1
+
+./apptainer.sh start boom ztf $(date -d "+1 day" +%Y%m%d) public
+
+sleep 79200 # 22h
